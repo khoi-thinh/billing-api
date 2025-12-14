ce58362c1097bc5ace0dd540b0a39f8a4a2566f4
Khoi Thinh <thinhduckhoi@gmail.com>
2025-12-14 11:24:21 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ app.get("/billing", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Billing API running on port ${PORT}`);
 });
